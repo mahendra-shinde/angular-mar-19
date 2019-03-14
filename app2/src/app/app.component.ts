e4f7efe18924cebb612874fd539c68a07b2b9036
Mahendra Shinde <MahendraShinde@synergetics-india.com>
2019-03-14 16:58:53 +0530

    Two Way bindings

--- app2/src/app/app.component.ts
+++ app2/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, NgModule } from '@angular/core';
+import { TouchSequence } from 'selenium-webdriver';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app2';
+  color: string = 'brown';
+  name: string = 'mahendra';
 }
