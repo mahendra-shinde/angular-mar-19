f8b1bcb65d7a88e1cf55f8a1a0cb9fd4154770cf
Mahendra Shinde <MahendraShinde@synergetics-india.com>
2019-03-15 15:01:55 +0530

    custom pipe (final)

--- pipe-demo/src/app/square-pipe.ts
+++ pipe-demo/src/app/square-pipe.ts
@@ -1,5 +1,8 @@
-import { PipeTransform } from '@angular/core';
+import { PipeTransform, Pipe } from '@angular/core';
 
+@Pipe({
+    name: 'square'
+})
 export class SquarePipe implements PipeTransform {
     transform(value: number, arg: Array<any>): any {
         return value * value;
