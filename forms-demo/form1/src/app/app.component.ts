e3aaddaa4586006f5824398ec1e1409d9c3271c4
Mahendra Shinde <MahendraShinde@synergetics-india.com>
2019-03-18 16:09:28 +0530

    reactive forms

--- forms-demo/form1/src/app/app.component.ts
+++ forms-demo/form1/src/app/app.component.ts
@@ -2,27 +2,32 @@ import { Component, OnInit } from '@angular/core';
 import {
   FormBuilder,
   FormGroup,
-  Validators
+  Validators,
+  FormControl
 } from '@angular/forms';
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'form1';
-
   constructor(fb: FormBuilder) {
     this.formControl = fb.group({
       'productName': ['', Validators.required]
     });
   }
-
-  ngOnInit(){
+  ngOnInit() {
   }
 
   formControl: FormGroup;
   productName: String;
+
+  onSubmit() {
+    if (this.formControl.invalid) {
+      console.log("Form is Invalid!");
+      return false;
+    }
+  }
 }
